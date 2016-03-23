83c401e15516619b9c7ae8080dd1129bfe78f4b0
bxagha <bxagha@gmail.com>
2016-03-23 22:38:14 +0300

    потоки

--- src/Java1/Thread/MainClass.java
+++ src/Java1/Thread/MainClass.java
@@ -6,23 +6,27 @@ package Java1.Thread;
 public class MainClass {
     static class Recipe implements Runnable{
 
+        void cook(){
+            for (int i = 0; i < 10; i++) {
+                System.err.println(Thread.currentThread().getName() + ": " + i);
+                try {
+                    Thread.sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
         @Override
         public void run() {
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.err.println("...");
-            System.err.println(Thread.currentThread().getName());
+            cook();
         }
     }
     public static void main(String[] args) throws InterruptedException {
-        Thread first = new Thread(new Recipe());
-        first.setName("first");
-        first.start();
-        Thread.sleep(3000);
-        System.err.println(first.getName());
+        for (int i = 0; i < 5; i++) {
+            Thread first = new Thread(new Recipe());
+            first.start();
+        }
         System.err.println(Thread.currentThread().getName());
     }
 }
